18f9771ccc69a83c5ab8fd9e9ab29f5b7969e715
Chanelp <mariannissanchez27@gmail.com>
2023-07-10 13:26:50 -0400

    Protegiendo el acceso: guards y token el LocalStorage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { environment } from '@environments/environment';
-import { switchMap } from 'rxjs';
+import { switchMap, tap } from 'rxjs';
+import { TokenService } from './token.service';
+import { ResponseLogin } from '@models/auth.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +13,14 @@ export class AuthService {
 
   apiURl = environment.API_URL;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
 
   login(email: string, password: string){
-    return this.http.post(`${this.apiURl}api/v1/auth/login`, {email, password});
+    return this.http.post<ResponseLogin>(`${this.apiURl}api/v1/auth/login`, {email, password})
+    .pipe(
+      tap(response =>{
+        this.tokenService.saveToken(response.access_token)
+      }));
   }
 
   register(name: string, email: string, password: string){
