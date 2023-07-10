405946325e8149daf6a2819f9f7df4417320610c
Chanelp <mariannissanchez27@gmail.com>
2023-07-10 15:50:29 -0400

    Guardando el token en Cookie

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {getCookie, setCookie, removeCookie} from 'typescript-cookie';
 
 @Injectable({
   providedIn: 'root'
@@ -8,16 +9,16 @@ export class TokenService {
   constructor() { }
 
   saveToken(token: string){
-    localStorage.setItem('token', token);
+    setCookie('token-trello', token);
   }
 
   getToken(){
-    const token = localStorage.getItem('token');
+    const token = getCookie('token-trello');
     return token;
   }
 
   removeToken(){
-    localStorage.removeItem('token');
+    removeCookie('token-trello');
   }
 
 
