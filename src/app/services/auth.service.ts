803b09f050166ce9008cb155158499b89d577231
Chanelp <mariannissanchez27@gmail.com>
2023-07-10 12:38:32 -0400

    Implementación del password recovery

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -31,4 +31,12 @@ export class AuthService {
       switchMap(() => this.login(email, password))
     )
   }
+
+  recovery(email: string){
+    return this.http.post(`${this.apiURl}api/v1/auth/recovery`, { email});
+  }
+
+  changePassword(token: string, newPassword: string){
+    return this.http.post(`${this.apiURl}api/v1/auth/change-password`, {token, newPassword})
+  }
 }
