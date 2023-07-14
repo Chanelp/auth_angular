6ab6ed67f51016c87bcafb688cfd8478e1142f02
Chanelp <mariannissanchez27@gmail.com>
2023-07-13 23:43:17 -0400

    Interceptor y contexto para enviar token en headers de peticion

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,62 +6,73 @@ import { BehaviorSubject, switchMap, tap } from 'rxjs';
 import { TokenService } from './token.service';
 import { ResponseLogin } from '@models/auth.model';
 import { User } from '@models/user.model';
+import { checkToken } from '@interceptors/token.interceptor';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   apiURl = environment.API_URL;
   user$ = new BehaviorSubject<User | null>(null);
 
   constructor(private http: HttpClient, private tokenService: TokenService) {}
 
-  login(email: string, password: string){
-    return this.http.post<ResponseLogin>(`${this.apiURl}api/v1/auth/login`, {email, password})
-    .pipe(
-      tap(response =>{
-        this.tokenService.saveToken(response.access_token)
-      }));
+  login(email: string, password: string) {
+    return this.http
+      .post<ResponseLogin>(`${this.apiURl}api/v1/auth/login`, {
+        email,
+        password,
+      })
+      .pipe(
+        tap((response) => {
+          this.tokenService.saveToken(response.access_token);
+        })
+      );
   }
 
-  register(name: string, email: string, password: string){
-    return this.http.post(`${this.apiURl}api/v1/auth/register`, {name, email, password});
+  register(name: string, email: string, password: string) {
+    return this.http.post(`${this.apiURl}api/v1/auth/register`, {
+      name,
+      email,
+      password,
+    });
   }
 
-  isAvailable(email: string){
-    return this.http.post<{isAvailable: boolean}>(`${this.apiURl}api/v1/auth/is-available`, {email});
+  isAvailable(email: string) {
+    return this.http.post<{ isAvailable: boolean }>(
+      `${this.apiURl}api/v1/auth/is-available`,
+      { email }
+    );
   }
 
-  registerAndLogin(name: string, email: string, password: string){
-    return this.register(name, email, password)
-    .pipe(
+  registerAndLogin(name: string, email: string, password: string) {
+    return this.register(name, email, password).pipe(
       switchMap(() => this.login(email, password))
-    )
+    );
   }
 
-  recovery(email: string){
-    return this.http.post(`${this.apiURl}api/v1/auth/recovery`, { email});
+  recovery(email: string) {
+    return this.http.post(`${this.apiURl}api/v1/auth/recovery`, { email });
   }
 
-  changePassword(token: string, newPassword: string){
-    return this.http.post(`${this.apiURl}api/v1/auth/change-password`, {token, newPassword})
+  changePassword(token: string, newPassword: string) {
+    return this.http.post(`${this.apiURl}api/v1/auth/change-password`, {
+      token,
+      newPassword,
+    });
   }
 
-  logout(){
+  logout() {
     this.tokenService.removeToken();
   }
 
-  getProfile(){
-    const token = this.tokenService.getToken();
-
-    let headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-
-    return this.http.get<User>(`${this.apiURl}api/v1/auth/profile`, {headers: headers})
-    .pipe(
-      tap(user => {
-        this.user$.next(user);
-      })
-    );
+  getProfile() {
+    return this.http
+      .get<User>(`${this.apiURl}api/v1/auth/profile`, { context: checkToken() })
+      .pipe(
+        tap((user) => {
+          this.user$.next(user);
+        })
+      );
   }
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -3,21 +3,19 @@ import { Injectable } from '@angular/core';
 import { TokenService } from './token.service';
 import { environment } from '@environments/environment';
 import { User } from '@models/user.model';
+import { checkToken } from '@interceptors/token.interceptor';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
-
   apiUrl = environment.API_URL;
 
-  constructor(private http:HttpClient, private tokenService: TokenService) { }
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
 
-  getUsers(){
-    const token = this.tokenService.getToken();
-
-    let headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-
-    return this.http.get<User[]>(`${this.apiUrl}api/v1/users`, {headers: headers});
+  getUsers() {
+    return this.http.get<User[]>(`${this.apiUrl}api/v1/users`, {
+      context: checkToken(),
+    });
   }
 }
